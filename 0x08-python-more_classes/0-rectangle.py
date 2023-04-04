2e65fcabc56c312d5fe48b43a84ff9ab28d1abdb
ayubsoft254 <henryoayub15@gmail.com>
2023-04-04 11:49:23 +0300

    task 0 error fixing

--- 0x08-python-more_classes/0-rectangle.py
+++ 0x08-python-more_classes/0-rectangle.py
@@ -1,7 +1,8 @@
 #!/usr/bin/python3
-""" rectangle class """
+# 0-rectangle.py
+"""Defines a Rectangle class."""
+
 
 class Rectangle:
-    """rectangle is represented here"""
+    """Represent a rectangle."""
     pass
-
